bfda78d258ba2a4bbd4807e5925ee924a27a8310
yesmin <ymslahoud@gmail.com>
2020-09-22 10:07:58 -0300

    API_URL change

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -31,7 +32,10 @@ class ListProviderService {
                 except_user_id: user_id,
             });
 
-            await this.cacheProvider.save(`providers-list:${user_id}`, users);
+            await this.cacheProvider.save(
+                `providers-list:${user_id}`,
+                classToClass(users),
+            );
         }
 
         return users;
